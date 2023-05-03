b955f38b0b9aca59bc5c6d7fdc85e7cc2f63f608
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2023-05-03 14:09:47 +0900

    Update get-max-min.hs with answer of ChatGPT

--- get-max-min.hs
+++ get-max-min.hs
@@ -1,11 +1,10 @@
 module Main where
+import Data.List (minimum, maximum)
 -- 与えられたリストの最大値と最小値を計算する関数を作成する。
 
 list = [1,2,3,4,5]
 
-range:: [Integer] -> (Integer, Integer)
-range [] = (0,0)
-range (x:xs) = foldl (\(max,min) x -> (if max < x then x else max, if min > x then x else min)) (x, x) xs
+minMax xs = (minimum xs, maximum xs)
 
 main = do
-  print $ range list
+  print $ minMax lis
