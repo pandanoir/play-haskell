bd46de6266bfd7224d7cc811a1fc39b5d6a9a553
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2023-05-03 15:05:49 +0900

    なるほど

--- sum-two-num2.hs
+++ sum-two-num2.hs
@@ -1,14 +1,19 @@
-module Main where
-import Text.Read
-import Control.Monad.Trans.Maybe ( MaybeT(..) )
-
 -- https://zenn.dev/tobi462/articles/4ae7658d126054 参考
+module Main where
+import Text.Read (readMaybe)
+import Control.Monad.Trans.Maybe ( MaybeT(..) )
+import Control.Monad.Trans.Class ( lift )
+
+readInt = readMaybe
+
 addIO = do
-  a <- MaybeT $ fmap readMaybe getLine
-  b <- MaybeT $ fmap readMaybe getLine
-  return $ a + b
+    lift $ putStrLn "enter number"
+    a <- MaybeT $ fmap readInt getLine
+    lift $ putStrLn "enter number"
+    b <- MaybeT $ fmap readInt getLine
+    return $ a + b
 
 main :: IO ()
 main = do
-  result <- runMaybeT addIO
-  print result
+    result <- runMaybeT addIO
+    print result
